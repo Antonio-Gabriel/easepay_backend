91a91143018b2d745cb9c3f4743c79b1fca23614
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-28 21:40:58 +0100

    entities: add validations

---
+++ src/domain/entities/phone_number.py
@@ -0,0 +1,32 @@
+class PhoneNumber:
+    def __init__(self, phone: int, user_id: int, id: int = None):
+        self.id = id
+        self.phone = phone
+        self.user = user_id
+
+        self.__contant_list = []
+
+    def insert_multiple_contact(self, *args):
+        """Insert multiple contact"""
+
+        self.__contant_list.append(*args)
+
+    def return_contacts(self):
+        """Return a list"""
+
+        return self.__contant_list[0]
+
+    def is_valid_phone_number(self):
+        """Returns true if the phone number is valid"""
+
+        return True if (len(str(self.phone)) == 9) else False
+
+    def is_valid_phone_number_of_list(self) -> bool:
+        """Returns true if the phone number is valid"""
+
+        for number in self.__contant_list:
+            for phone in number:
+                if not len(str(phone)) == 9:
+                    return False
+
+        return True
