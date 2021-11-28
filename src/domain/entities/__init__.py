2730b41b8f04ebfa0b3da2a165e496e39d24aa36
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-28 21:10:00 +0100

    entities: add user attr and validate email address

--- src/domain/entities/__init__.py
+++ src/domain/entities/__init__.py
@@ -1,1 +1,2 @@
 from .account import Account
+from .user import User
---
+++ src/domain/entities/user.py
@@ -0,0 +1,12 @@
+import re
+
+
+class User:
+    def __init__(self, full_name: str, email: str, id: int = None):
+        self.id = id
+        self.full_name = full_name
+        self.email = email
+
+    def is_valid_email_address(self) -> bool:
+        expretion = r"^[a-zA-Z0-9._-]+@([a-z0-9]+)(\.[a-z]{2,3})+$"
+        return True if re.search(expretion, self.email) is not None else False
