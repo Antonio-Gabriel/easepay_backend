b9797cc4f49bb385749e0405c363cb7a1218bdb0
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-28 21:54:38 +0100

    entities: add deposit and withdraw in walleet

---
+++ src/domain/entities/wallet.py
@@ -0,0 +1,16 @@
+class Wallet:    
+    def __init__(self, amount: float, id: int = None):
+        self.id = id
+        self.amount = amount
+    
+    def deposit(self, amount_to_deposit: float):
+        """ Add ammount to the wallet"""
+
+        self.amount += amount_to_deposit
+        return self.amount
+
+    def with_draw(self, amount_to_withdraw: float):
+        """ remove amount to the wallet"""
+
+        self.amount -= amount_to_withdraw
+        return self.amount
