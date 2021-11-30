cb59d8b8631a69ed9c1c0e135eb38d156a80f926
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-30 21:58:28 +0100

    entiti: user

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -1,14 +1,24 @@
 import re
+from typing import Type
+
+from .phone_number import PhoneNumber
 
 
 class User:
-    def __init__(self, full_name: str, email: str, id: int = None):
+    def __init__(
+        self,
+        full_name: str,
+        email: str,
+        phone_number: Type[PhoneNumber],
+        id: int = None,
+    ):
         self.id = id
         self.full_name = full_name
         self.email = email
+        self.phone_number = phone_number
 
     def is_valid_email_address(self) -> bool:
-        """ Verify if the email address is valid"""
+        """Verify if the email address is valid"""
 
         expretion = r"^[a-zA-Z0-9._-]+@([a-z0-9]+)(\.[a-z]{2,3})+$"
         return True if re.search(expretion, self.email) is not None else False
