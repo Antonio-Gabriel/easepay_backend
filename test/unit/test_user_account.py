c4cae9fff388efb4ee097668da264e3435c7a656
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-30 21:59:25 +0100

    test: insert multiple contacts

--- test/unit/test_user_account.py
+++ test/unit/test_user_account.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
 
-from src.domain.entities import UserAccount, Account, User, Wallet
+from src.domain.entities import UserAccount, Account, User, Wallet, PhoneNumber
+
 
 class TestUserAccount(TestCase):
-
     def test_is_null_or_empty_attr(self):
-        
-        account = Account(99873, 'a')
-        user = User('a', 'a')
+
+        phone_number = PhoneNumber(9)
+        account = Account(99873, "a")
+        user = User("a", "a", phone_number)
         wallet = Wallet()
         user_account = UserAccount(account, user, wallet)
         respose = user_account.is_null_or_empty_attr()
