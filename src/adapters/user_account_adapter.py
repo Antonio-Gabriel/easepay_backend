cf917a641aaac9ddc3e7f0d82cd0d71dbedb65d1
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-01 01:06:38 +0100

    adapter:  add ammount on user account udapter

--- src/adapters/user_account_adapter.py
+++ src/adapters/user_account_adapter.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import List
 from ..domain.entities import UserAccount, User, Account, Wallet, PhoneNumber
 
@@ -5,12 +6,17 @@ from ..domain.entities import UserAccount, User, Account, Wallet, PhoneNumber
 class UserAccountAdapter:
     @staticmethod
     def create(
-        permition_code: int, password: str, full_name: str, email: str, phone: List[int]
+        permition_code: int,
+        password: str,
+        full_name: str,
+        email: str,
+        phone: List[int],
+        amonut: Decimal = 0.00,
     ):
         """Useraccount adapter"""
 
         return UserAccount(
             account=Account(permition_code, password),
             user=User(full_name, email, PhoneNumber(phone)),
-            wallet=Wallet(),
+            wallet=Wallet(amonut),
         )
