447a74fbeacb0f42a812e22e3e06ff053d05b2dc
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-01 01:07:16 +0100

    entities: remove methods in phone_number

--- src/domain/entities/phone_number.py
+++ src/domain/entities/phone_number.py
@@ -4,19 +4,7 @@ from typing import List
 class PhoneNumber:
     def __init__(self, phone: List[int], id: int = None):
         self.id = id
-        self.phone = phone
-
-        self.__contant_list = []
-
-    def insert_multiple_contact(self, *args):
-        """Insert multiple contact"""
-
-        self.__contant_list.append(*args)
-
-    def return_contacts(self):
-        """Return a list"""
-
-        return self.__contant_list[0]
+        self.phone = phone        
 
     def is_valid_phone_number(self):
         """Returns true if the phone number is valid"""
@@ -26,9 +14,8 @@ class PhoneNumber:
     def is_valid_phone_number_of_list(self) -> bool:
         """Returns true if the phone number is valid"""
 
-        for number in self.__contant_list:
-            for phone in number:
-                if not len(str(phone)) == 9:
-                    return False
+        for number in self.phone:
+            if not len(str(number)) == 9:
+                return False
 
         return True
