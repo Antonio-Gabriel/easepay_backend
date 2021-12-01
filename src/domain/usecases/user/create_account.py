0881cc8948e40fe05907e47523a9691dad757176
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-01 01:08:08 +0100

    usecases: update create account method

--- src/domain/usecases/user/create_account.py
+++ src/domain/usecases/user/create_account.py
@@ -10,7 +10,7 @@ class CreateAccount:
     def __init__(self, user_account_interface: Type[IAccountRepository]):
         self.__iuser_account = user_account_interface
 
-    def execute(self, user_account: Type[IUserAccountRequestDto]):
+    def execute(self, user_account: Type[IUserAccountRequestDto]) -> list:
         """Create account
 
         Args:
@@ -24,7 +24,11 @@ class CreateAccount:
             user_account.user.full_name,
             user_account.user.email,
             user_account.user.phone_number.phone,
+            user_account.wallet.amount,
         )
+
+        adapter_instance.created_at = user_account.created_at
+        adapter_instance.updated_at = user_account.updated_at
 
         if not adapter_instance.is_null_or_empty_attr():
             raise Exception("Verify empty value")
@@ -35,4 +39,7 @@ class CreateAccount:
         if not user_account.user.is_valid_email_address():
             raise Exception("Invalid email, pleace check the email...")
 
-        self.__iuser_account.create_account(adapter_instance)
+        if not user_account.user.phone_number.is_valid_phone_number_of_list():
+            raise Exception("Phone number is invalid")
+
+        return self.__iuser_account.create_account(adapter_instance)
