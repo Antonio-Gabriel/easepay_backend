b0faf6ea0967d0e41d43a6b78bad9979493414bc
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-01 01:09:08 +0100

    repository: add update user account

--- src/infra/repository/user_account_in_memory.py
+++ src/infra/repository/user_account_in_memory.py
@@ -1,20 +1,66 @@
-from typing import Type
-from ...domain.repositories import IAccountRepository
+from datetime import datetime
+from typing import Type, Union
+from uuid import uuid4
 
+from ...domain.repositories import IAccountRepository
 from ...domain.entities import UserAccount
 
 
 class UserAccountInMemoryRepository(IAccountRepository):
     def __init__(self):
-        pass
+        self.__storage = [
+            {
+                "id": str(uuid4()),
+                "name": "Antonio Gabriel",
+                "email": "antoniogabriel@gmail.com",
+                "password": b"7bd609ad1b5a0c4a52c012263801539c1235cf4f",
+                "permition_code": 99834,
+                "phone": [989834555, 982345222],
+                "wallet": 0,
+                "state": True,
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+            }
+        ]
 
-    def create_account(self, user_account: Type[UserAccount]) -> UserAccount:
+    def create_account(self, user_account: Type[UserAccount]) -> Union[list, dict]:
         """Create a new account"""
 
-        hash_pass = user_account.account.password_encrypt()
-        print(user_account.user.full_name, hash_pass)
+        try:
+            hash_pass = user_account.account.password_encrypt()
+            self.__storage.append(
+                {
+                    "id": str(uuid4()),
+                    "name": user_account.user.full_name,
+                    "email": user_account.user.email,
+                    "password": hash_pass,
+                    "permition_code": user_account.account.permition_code,
+                    "phone": user_account.user.phone_number.phone,
+                    "wallet": user_account.wallet.amount,
+                    "state": user_account.state,
+                    "created_at": user_account.created_at,
+                    "updated_at": user_account.updated_at,
+                }
+            )
 
-    def update_account(user_account: Type[UserAccount]) -> UserAccount:
+            return self.__storage
+        except Exception as ex:
+            return {"error": ex.args}
+
+    def update_account(self, user_account: Type[UserAccount]) -> UserAccount:
         """Update account"""
 
-        pass
+        hash_pass = user_account.account.password_encrypt()        
+        object_assigned = {
+            **self.__storage[0],
+            "name": user_account.user.full_name,
+            "email": user_account.user.email,
+            "password": hash_pass,
+            "permition_code": user_account.account.permition_code,
+            "phone": user_account.user.phone_number.phone,
+            "updated_at": user_account.updated_at,
+        }
+
+        self.__storage[0] = object_assigned
+
+        return self.__storage
