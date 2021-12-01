af9c9c4d49673d418dfeb58af8347935c4b4caab
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-01 01:53:19 +0100

    repository: alter user account repository

--- src/domain/repositories/user_account_repository.py
+++ src/domain/repositories/user_account_repository.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Type
+from typing import List, Type, Union
 
 from ..entities import UserAccount
 
 
 class IAccountRepository(ABC):
     @abstractmethod
-    def create_account(user_account: Type[UserAccount]) -> UserAccount:
+    def create_account(
+        user_account: Type[UserAccount],
+    ) -> Union[List[UserAccount], dict]:
         """Create a new account"""
 
         raise NotImplementedError("This method is not implemented")
@@ -14,5 +16,11 @@ class IAccountRepository(ABC):
     @abstractmethod
     def update_account(user_account: Type[UserAccount]) -> UserAccount:
         """Update account"""
+
+        raise NotImplementedError("This method is not implemented")
+
+    @abstractmethod
+    def forgot_account(email_address: str) -> List[UserAccount]:
+        """Forget account"""
 
         raise NotImplementedError("This method is not implemented")
